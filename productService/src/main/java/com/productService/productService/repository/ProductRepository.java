e9a88db5ff62ba0e0461ccde811ff7e7c9c05df7
thushanm <122750947+thushanm@users.noreply.github.com>
2024-09-08 17:22:13 +0530

    change dir

--- productService/src/main/java/com/productService/productService/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.productService.productService.repository;
-
-
-import com.productService.productService.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product,String> {
-}
