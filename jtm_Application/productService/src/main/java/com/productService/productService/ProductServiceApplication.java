2c4a3d907051c9c018ee438c965dff41d2b43c1e
thushanmm <thushan_madhushara@cypsolabs.com>
2024-11-30 22:51:19 +0530

    change some

--- jtm_Application/productService/src/main/java/com/productService/productService/ProductServiceApplication.java
+++ jtm_Application/productService/src/main/java/com/productService/productService/ProductServiceApplication.java
@@ -1,7 +1,9 @@
 package com.productService.productService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductServiceApplication {
@@ -10,5 +12,11 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 		System.out.println("oky");
 	}
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+
 
 }
