53b48d1cb56acd737088a839f804b48a08cf9fc8
thushanmm <thushan_madhushara@cypsolabs.com>
2024-11-30 23:14:20 +0530

    changes some

--- jtm_Application/orderService/src/main/java/com/orderService/orderService/OrderServiceApplication.java
+++ jtm_Application/orderService/src/main/java/com/orderService/orderService/OrderServiceApplication.java
@@ -1,13 +1,20 @@
 package com.orderService.orderService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+
+		return modelMapper();
 	}
 
 }
--- jtm_Application/orderService/src/main/java/com/orderService/orderService/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.orderService.orderService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
